1880a32fc1bdc2bc899759771ed471f09846e6f0
myh <huang.minya@northeastern.edu>
2024-10-11 20:57:40 -0400

    Initial Commit

---
+++ Fetch/Fetch.h
@@ -0,0 +1,18 @@
+//
+//  Fetch.h
+//  Fetch
+//
+//  Created by Min-Yang Huang on 2024/10/11.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Fetch.
+FOUNDATION_EXPORT double FetchVersionNumber;
+
+//! Project version string for Fetch.
+FOUNDATION_EXPORT const unsigned char FetchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Fetch/PublicHeader.h>
+
+
