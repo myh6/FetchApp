76fc9ce7f700b0efb4737f078258be5b4b7f067e
myh <huang.minya@northeastern.edu>
2024-10-11 21:31:51 -0400

    `RemoteFeedLoader` `load` commands request data from URL

--- FetchTests/RemoteFeedLoaderTests.swift
+++ FetchTests/RemoteFeedLoaderTests.swift
@@ -6,17 +6,33 @@
 //
  
 import XCTest
+import Fetch
 
-class RemoteFeedLoader {
+protocol FeedLoader {
+    typealias Result = Swift.Result<[FeedItem], Error>
+    func load(completion: @escaping (Result) -> Void)
+}
+
+class RemoteFeedLoader: FeedLoader {
     let client: HTTPClient
+    let url: URL
     
-    init(client: HTTPClient) {
+    init(client: HTTPClient, url: URL) {
         self.client = client
+        self.url = url
+    }
+    
+    func load(completion: @escaping (FeedLoader.Result) -> Void) {
+        client.get(from: url)
     }
 }
 
 class HTTPClient {
     var requestedURL: URL?
+    
+    func get(from url: URL) {
+        requestedURL = url
+    }
 }
 
 final class RemoteFeedLoaderTests: XCTestCase {
@@ -27,11 +43,20 @@ final class RemoteFeedLoaderTests: XCTestCase {
         XCTAssertNil(client.requestedURL)
     }
     
+    func test_load_requestsDataFromURL() {
+        let url = URL(string: "https://any-url.com")!
+        let (sut, client) = makeSUT(url: url)
+        
+        sut.load { _ in }
+        
+        XCTAssertEqual(client.requestedURL, url)
+    }
+    
     
     //MARK: - Helpers
-    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: RemoteFeedLoader, client: HTTPClient) {
+    private func makeSUT(url: URL = URL(string: "https://example.com")!, file: StaticString = #file, line: UInt = #line) -> (sut: RemoteFeedLoader, client: HTTPClient) {
         let client = HTTPClient()
-        let sut = RemoteFeedLoader(client: client)
+        let sut = RemoteFeedLoader(client: client, url: url)
         trackForMemoryLeaks(sut, file: file, line: line)
         trackForMemoryLeaks(client, file: file, line: line)
         return (sut, client)
