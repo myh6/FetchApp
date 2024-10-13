2657ab34204782e9f2ebf0f5507c32eb14ca7116
myh <huang.minya@northeastern.edu>
2024-10-13 17:04:53 -0400

    `RemoteRecipeImageDataLoader` `loadImageDataFromURL` delivers error on client error

--- FetchTests/RemoteRecipeImageDataLoaderTests.swift
+++ FetchTests/RemoteRecipeImageDataLoaderTests.swift
@@ -26,8 +26,12 @@ class RemoteRecipeImageDataLoader {
         self.client = client
     }
     
-    func loadImageData(from url: URL, completion: @escaping () -> Void) {
-        client.get(from: url) { _ in }
+    func loadImageData(from url: URL, completion: @escaping (RecipeImageDataLoader.Result) -> Void) {
+        client.get(from: url) { result in
+            if case let .failure(error) = result {
+                completion(.failure(error))
+            }
+        }
     }
 }
 
@@ -35,26 +39,36 @@ final class RemoteRecipeImageDataLoaderTests: XCTestCase {
     func test_init_doesNotPerformURLRequest() {
         let (_, client) = makeSUT()
         
-        XCTAssertTrue(client.requestedURLs.isEmpty)
+        XCTAssertTrue(client.requestedURL.isEmpty)
     }
     
     func test_loadImageDataFromURL_performsURLRequest() {
         let url = anyURL()
         let (sut, client) = makeSUT()
         
-        sut.loadImageData(from: url) {}
+        sut.loadImageData(from: url) { _ in }
         
-        XCTAssertEqual(client.requestedURLs, [url])
+        XCTAssertEqual(client.requestedURL, [url])
     }
     
     func test_loadImageDataFromURLTwice_requestsDataFromURLRequestTwice() {
         let url = anyURL()
         let (sut, client) = makeSUT()
         
-        sut.loadImageData(from: url) {}
-        sut.loadImageData(from: url) {}
+        sut.loadImageData(from: url) { _ in}
+        sut.loadImageData(from: url) { _ in }
         
-        XCTAssertEqual(client.requestedURLs, [url, url])
+        XCTAssertEqual(client.requestedURL, [url, url])
+    }
+    
+    func test_loadImageDataFromURL_delviersErrorOnClientError() {
+        let (sut, client) = makeSUT()
+        let clientError = NSError(domain: "any error", code: 0)
+        
+        
+        expect(sut, toCompleteWith: .failure(clientError)) {
+            client.complete(with: clientError)
+        }
     }
     
     //MARK: - Helpers
@@ -66,11 +80,43 @@ final class RemoteRecipeImageDataLoaderTests: XCTestCase {
         return (sut, client)
     }
     
-    private class HTTPClientSpy: HTTPClient {
-        var requestedURLs = [URL]()
+    class HTTPClientSpy: HTTPClient {
+        private var messages = [(requestedURL: URL, completion: (HTTPClient.Result) -> Void)]()
+        var requestedURL: [URL] {
+            messages.map(\.requestedURL)
+        }
         
         func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
-            requestedURLs.append(url)
+            messages.append((url, completion))
         }
+        
+        func complete(with error: NSError, at index: Int = 0) {
+            messages[index].completion(.failure(error))
+        }
+        
+        func complete(withStatusCode code: Int, data: Data, at index: Int = 0) {
+            let response = HTTPURLResponse(url: requestedURL[index], statusCode: code, httpVersion: nil, headerFields: nil)!
+            messages[index].completion(.success((data, response)))
+        }
+    }
+    
+    private func expect(_ sut: RemoteRecipeImageDataLoader, toCompleteWith expectedResult: RecipeImageDataLoader.Result, when action: () -> Void, file: StaticString = #file, line: UInt = #line) {
+        let url = anyURL()
+        let exp = expectation(description: "Wait for completion")
+        
+        sut.loadImageData(from: url) { receivedResult in
+            switch (receivedResult, expectedResult) {
+            case let (.success(receivedData), .success(expectedData)):
+                XCTAssertEqual(receivedData, expectedData)
+            case let (.failure(receivedError), .failure(expectedError)):
+                XCTAssertEqual(receivedError as NSError, expectedError as NSError)
+            default:
+                XCTFail("Expected to complete with \(expectedResult), but received \(receivedResult) instead.")
+            }
+            exp.fulfill()
+        }
+        
+        action()
+        wait(for: [exp], timeout: 1.0)
     }
 }
