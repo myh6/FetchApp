3a1b04e7132e64bdd86a96e0e753c1b12cb1a9be
myh <huang.minya@northeastern.edu>
2024-10-13 22:18:23 -0400

    `LocalRecipeImageDataLoader` does not message store upon creation

--- FetchTests/Image/LocalRecipeImageDataLoaderTests.swift
+++ FetchTests/Image/LocalRecipeImageDataLoaderTests.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+import XCTest
+
+class LocalRecipeImageDataLoader {}
+
+class RecipeImageDataStore {
+    var receivedMessage = [Any]()
+    
+    
+}
+
+class LocalRecipeImageDataLoaderTests: XCTestCase {
+    func test_init_doesNotMessageStoreUponCreation() {
+        let (_, store) = makeSUT()
+        
+        XCTAssertTrue(store.receivedMessage.isEmpty)
+    }
+    
+    //MARK: - Helpers
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: LocalRecipeImageDataLoader, store: RecipeImageDataStore) {
+        let sut = LocalRecipeImageDataLoader()
+        let store = RecipeImageDataStore()
+        trackForMemoryLeaks(sut, file: file, line: line)
+        trackForMemoryLeaks(store, file: file, line: line)
+        return (sut, store)
+    }
+}
