97c9370b7a07368d246048ae5611b8aef55f2366
myh <huang.minya@northeastern.edu>
2024-10-14 21:51:55 -0400

    `CoreDataRecipeImageStore` `retrieveDataForURL` delivers empty when empty

---
+++ Fetch/Image/Infrastructure/CoreDataRecipeImageStore.swift
@@ -0,0 +1,55 @@
+//
+//  CoreDataRecipeImageStore.swift
+//  Fetch
+//
+//  Created by Min-Yang Huang on 2024/10/14.
+//
+
+import CoreData
+
+public class CoreDataRecipeImageStore {
+    private static let modelName = "RecipeStore"
+    private static let model = NSManagedObjectModel.with(name: modelName, in: Bundle(for: CoreDataRecipeImageStore.self))
+    
+    private let container: NSPersistentContainer
+    private let context: NSManagedObjectContext
+    
+    enum StoreError: Error {
+        case modelNotFound
+        case failedToLoadPersistentContainer(Error)
+    }
+    
+    public init(storeURL: URL) throws {
+        guard let model = CoreDataRecipeImageStore.model else {
+            throw StoreError.modelNotFound
+        }
+        do {
+            container = try NSPersistentContainer.load(name: CoreDataRecipeImageStore.modelName, model: model, url: storeURL)
+            context = container.newBackgroundContext()
+        } catch {
+            throw StoreError.failedToLoadPersistentContainer(error)
+        }
+    }
+}
+
+extension NSPersistentContainer {
+    static func load(name: String, model: NSManagedObjectModel, url: URL) throws -> NSPersistentContainer {
+        let description = NSPersistentStoreDescription(url: url)
+        let container = NSPersistentContainer(name: name, managedObjectModel: model)
+        container.persistentStoreDescriptions = [description]
+        
+        var loadError: Swift.Error?
+        container.loadPersistentStores { loadError = $1 }
+        try loadError.map { throw $0 }
+        
+        return container
+    }
+}
+
+extension NSManagedObjectModel {
+    static func with(name: String, in bundle: Bundle) -> NSManagedObjectModel? {
+        return bundle
+            .url(forResource: name, withExtension: "momd")
+            .flatMap { NSManagedObjectModel(contentsOf: $0) }
+    }
+}
--- FetchTests/Image/CoreDataRecipeImageStoreTests.swift
+++ FetchTests/Image/CoreDataRecipeImageStoreTests.swift
@@ -7,7 +7,46 @@
 
 import Foundation
 import XCTest
+import Fetch
+
+extension CoreDataRecipeImageStore {
+    func retrieve(dataForURL url: URL, completion: @escaping (RecipeImageDataStore.RetrievalResult) -> Void) {
+        completion(.success(.none))
+    }
+}
 
 class CoreDataRecipeImageStoreTests: XCTestCase {
     
+    func test_retrieveDataForURL_deliversNotFoundWhenEmpty() {
+        let sut = makeSUT()
+        
+        expect(sut, toCompleteRetrievalWith: .success(.none), for: anyURL())
+    }
+    
+    //MARK: - Helpers
+    private func makeSUT() -> CoreDataRecipeImageStore {
+        let storeURL = URL(filePath: "/dev/null")
+        let sut = try! CoreDataRecipeImageStore(storeURL: storeURL)
+        trackForMemoryLeaks(sut)
+        return sut
+    }
+    
+    private func expect(_ sut: CoreDataRecipeImageStore, toCompleteRetrievalWith expectedResult: RecipeImageDataStore.RetrievalResult, for url: URL, file: StaticString = #file, line: UInt = #line) {
+        let exp = expectation(description: "Wait for retrieval")
+        sut.retrieve(dataForURL: url) { receivedResult in
+            switch (receivedResult, expectedResult) {
+            case let (.success(receivedData), .success(expectedData)):
+                XCTAssertEqual(receivedData, expectedData, file: file, line: line)
+                
+            case let (.failure(receivedError as NSError), .failure(expectedError as NSError)):
+                XCTAssertEqual(receivedError, expectedError, file: file, line: line)
+                
+            default:
+                XCTFail("Expected to complete with \(expectedResult), but got \(receivedResult) instead")
+            }
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+    }
 }
