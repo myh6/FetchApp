ef06ccfb661165c7ae86574c59b42979a7a3884b
myh <huang.minya@northeastern.edu>
2024-10-15 18:17:10 -0400

    Remove `FetchApp` as a submodule and added it as a regular file

---
+++ FetchApp/FetchApp/FetchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchAppApp.swift
+//  FetchApp
+//
+//  Created by Min-Yang Huang on 2024/10/15.
+//
+
+import SwiftUI
+
+@main
+struct FetchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
