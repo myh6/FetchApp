a40a6cb646b365182e4d16a6eeb3c4d47606c9e1
myh <huang.minya@northeastern.edu>
2024-10-16 18:21:51 -0400

    `ImageLoaderWithFallbackComposite` `cancel` cancels both primary and fallback tasks

--- FetchApp/FetchAppTests/ImageLoaderWithFallbackCompositeTests.swift
+++ FetchApp/FetchAppTests/ImageLoaderWithFallbackCompositeTests.swift
@@ -45,14 +45,14 @@ final class ImageLoaderWithFallbackCompositeTests: XCTestCase {
     func test_loadImageData_deliversPrimaryResultOnPrimarySuccess() {
         let primaryImage = Data("primary data".utf8)
         let fallbackImage = Data("fallback data".utf8)
-        let sut = makeSUT(primaryResult: .success(primaryImage), fallbackResult: .success(fallbackImage))
+        let (sut, _, _) = makeSUT(primaryResult: .success(primaryImage), fallbackResult: .success(fallbackImage))
         
         expect(sut, toCompleteWith: .success(primaryImage))
     }
     
     func test_loadImageData_delviersFallbackResultOnPrimaryFailure() {
         let fallbackImage = Data("fallback data".utf8)
-        let sut = makeSUT(
+        let (sut, _, _) = makeSUT(
             primaryResult: .failure(LocalRecipeImageDataLoader.LoadError.failed),
             fallbackResult: .success(fallbackImage))
         
@@ -60,20 +60,34 @@ final class ImageLoaderWithFallbackCompositeTests: XCTestCase {
     }
     
     func test_loadImageData_deliversErrorOnFallbackFailure() {
-        let sut = makeSUT(primaryResult: .failure(LocalRecipeImageDataLoader.LoadError.failed), fallbackResult: .failure(RemoteRecipeImageDataLoader.Error.connectivity))
+        let (sut, _, _) = makeSUT(primaryResult: .failure(LocalRecipeImageDataLoader.LoadError.failed), fallbackResult: .failure(RemoteRecipeImageDataLoader.Error.connectivity))
         
         expect(sut, toCompleteWith: .failure(RemoteRecipeImageDataLoader.Error.connectivity))
     }
     
+    func test_cancel_cancelsBothPrimaryAndFallbackTasks() {
+        let (sut, primaryTask, fallbackTask) = makeSUT(primaryResult: .failure(NSError(domain: "any error", code: 0)), fallbackResult: .success(Data()))
+        
+        let task = sut.loadImageData(from: URL(string: "https://any-url.com")!) { _ in }
+        
+        task.cancel()
+        XCTAssertTrue(primaryTask.isCancelled)
+        XCTAssertTrue(fallbackTask.isCancelled)
+    }
+    
     //MARK: - Helpers
-    private func makeSUT(primaryResult: RecipeImageDataLoader.Result, fallbackResult: RecipeImageDataLoader.Result, file: StaticString = #file, line: UInt = #line) -> ImageLoaderWithFallbackComposite {
-        let primary = LoaderStub(result: primaryResult)
-        let fallback = LoaderStub(result: fallbackResult)
+    private func makeSUT(primaryResult: RecipeImageDataLoader.Result, fallbackResult: RecipeImageDataLoader.Result, file: StaticString = #file, line: UInt = #line) -> (sut: ImageLoaderWithFallbackComposite, primaryTask: LoaderStub.TaskSpy, fallbackTask: LoaderStub.TaskSpy) {
+        let primaryTask = LoaderStub.TaskSpy()
+        let fallbackTask = LoaderStub.TaskSpy()
+        let primary = LoaderStub(result: primaryResult, task: primaryTask)
+        let fallback = LoaderStub(result: fallbackResult, task: fallbackTask)
         let sut = ImageLoaderWithFallbackComposite(primary: primary, fallback: fallback)
+        trackForMemoryLeaks(primaryTask, file: file, line: line)
+        trackForMemoryLeaks(fallbackTask, file: file, line: line)
         trackForMemoryLeaks(primary, file: file, line: line)
         trackForMemoryLeaks(fallback, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
-        return sut
+        return (sut, primaryTask, fallbackTask)
     }
     
     private func expect(_ sut: ImageLoaderWithFallbackComposite, toCompleteWith expectedResult: RecipeImageDataLoader.Result, file: StaticString = #file, line: UInt = #line) {
@@ -96,19 +110,25 @@ final class ImageLoaderWithFallbackCompositeTests: XCTestCase {
     
     private class LoaderStub: RecipeImageDataLoader {
         private let result: RecipeImageDataLoader.Result
+        private let task: TaskSpy
         
-        private struct Task: RecipeImageDataLoaderTask {
-            func cancel() {}
+        class TaskSpy: RecipeImageDataLoaderTask {
+            var isCancelled: Bool = false
+            func cancel() {
+                isCancelled = true
+            }
         }
         
-        init(result: RecipeImageDataLoader.Result) {
+        init(result: RecipeImageDataLoader.Result, task: TaskSpy) {
             self.result = result
+            self.task = task
         }
         
         func loadImageData(from url: URL, completion: @escaping (RecipeImageDataLoader.Result) -> Void) -> any RecipeImageDataLoaderTask {
             completion(result)
-            return Task()
+            return task
         }
+        
     }
 }
 
