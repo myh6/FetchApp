5e67724db7ae4433722172bbababfdcc2da8a973
myh <huang.minya@northeastern.edu>
2024-10-16 14:57:07 -0400

    `ImageLoaderWithFallbackComposite` `loadImageData` commands delivers primary result on primary success

--- FetchApp/FetchAppTests/ImageLoaderWithFallbackCompositeTests.swift
+++ FetchApp/FetchAppTests/ImageLoaderWithFallbackCompositeTests.swift
@@ -8,13 +8,36 @@
 import XCTest
 import Fetch
 
-class ImageLoaderWithFallbackComposite {
+class ImageLoaderWithFallbackComposite: RecipeImageDataLoader {
     let primary: RecipeImageDataLoader
     let fallback: RecipeImageDataLoader
+    
+    private struct CompositeTask: RecipeImageDataLoaderTask {
+        fileprivate var primary: RecipeImageDataLoaderTask?
+        fileprivate var fallback: RecipeImageDataLoaderTask?
+        
+        func cancel() {
+            primary?.cancel()
+            fallback?.cancel()
+        }
+    }
     
     init(primary: RecipeImageDataLoader, fallback: RecipeImageDataLoader) {
         self.primary = primary
         self.fallback = fallback
+    }
+    
+    func loadImageData(from url: URL, completion: @escaping (RecipeImageDataLoader.Result) -> Void) -> any RecipeImageDataLoaderTask {
+        var compositeTask = CompositeTask()
+        compositeTask.primary = primary.loadImageData(from: url) { result in
+            if case .success(let imageData) = result {
+                completion(.success(imageData))
+            } else {
+                compositeTask.fallback = self.fallback.loadImageData(from: url) { result in }
+            }
+        }
+        
+        return compositeTask
     }
 }
 
@@ -22,14 +45,16 @@ final class ImageLoaderWithFallbackCompositeTests: XCTestCase {
 
     func test_loadImageData_deliversPrimaryResultOnPrimarySuccess() {
         let url = URL(string: "https://example.com/primary")!
-        let primary = LoaderStub()
-        let fallback = LoaderStub()
-        let sut =  (primary: primary, fallback: fallback)
+        let primaryImage = Data("primary data".utf8)
+        let fallbackImage = Data("fallback data".utf8)
+        let primary = LoaderStub(result: .success(primaryImage))
+        let fallback = LoaderStub(result: .success(fallbackImage))
+        let sut = ImageLoaderWithFallbackComposite(primary: primary, fallback: fallback)
         
         let exp = expectation(description: "Wait for copletion")
         var receivedImageData: Data?
         
-        sut.loadImageData(from: url) { result in
+        _ = sut.loadImageData(from: url) { result in
             if case let .success(data) = result {
                 receivedImageData = data
             } else {
@@ -45,8 +70,19 @@ final class ImageLoaderWithFallbackCompositeTests: XCTestCase {
     //MARK: - Helpers
     
     private class LoaderStub: RecipeImageDataLoader {
+        private let result: RecipeImageDataLoader.Result
+        
+        private struct Task: RecipeImageDataLoaderTask {
+            func cancel() {}
+        }
+        
+        init(result: RecipeImageDataLoader.Result) {
+            self.result = result
+        }
+        
         func loadImageData(from url: URL, completion: @escaping (RecipeImageDataLoader.Result) -> Void) -> any RecipeImageDataLoaderTask {
-            <#code#>
+            completion(result)
+            return Task()
         }
     }
 }
